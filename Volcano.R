b20df495c27c2e568171ddf6f969d7b62c4aa9df
Seyonne989 <34789958+Seyonne989@users.noreply.github.com>
2017-12-23 12:29:37 +0200

    Update Volcano.R

--- Volcano.R
+++ Volcano.R
@@ -2,10 +2,6 @@ z <- 2 * volcano
 x <- 10 * (1:nrow(z))  
 y <- 10 * (1:ncol(z))   
 
-z0 <- min(z) - 20
-z <- rbind(z0, cbind(z0, z, z0), z0)
-x <- c(min(x) - 1e-10, x, max(x) + 1e-10)
-y <- c(min(y) - 1e-10, y, max(y) + 1e-10)
 
 fill <- matrix("green3", nr = nrow(z)-1, nc = ncol(z)-1)
 fill[ , i2 <- c(1,ncol(fill))] <- "gray"
